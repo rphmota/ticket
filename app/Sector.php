85a413f0fa576d8e77c033b44f6d38f309be13a6
Raphael Mota <rphmota@gmail.com>
2020-07-20 00:01:23 -0300

    modelo de setor
    adicionado modelo de setor com as diretivas basicas

---
+++ app/Sector.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sector extends Model
+{
+    protected $table = 'sectors';
+
+    protected $fillable = [
+        'name', 'endereco', 'telefone', 'responsavel', 'descricao', 'ativo'
+    ];
+
+}
