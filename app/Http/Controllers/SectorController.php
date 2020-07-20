3d9628c33f95e2d87e4407872193bacd63272c68
Raphael Mota <rphmota@gmail.com>
2020-07-20 00:02:04 -0300

    Create SectorController.php
    Controller de setor criado e finalizado o metodo store, create e index

---
+++ app/Http/Controllers/SectorController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Sector;
+use Illuminate\Http\Request;
+
+class SectorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('front.sectors.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('front.sectors.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Sector::create($request->all());
+        return redirect()->route('sectors.index')
+            ->with('success','Product created successfully.');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Sector  $sector
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Sector $sector)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Sector  $sector
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Sector $sector)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Sector  $sector
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sector $sector)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Sector  $sector
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Sector $sector)
+    {
+        //
+    }
+}
