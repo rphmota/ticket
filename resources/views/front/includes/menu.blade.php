2133cfa8aaeee9f8343e8c539dc713c602c6c443
Raphael Mota <rphmota@gmail.com>
2020-07-20 00:04:03 -0300

    Particionando Admin-Lte
    Adicionando as diretivas de assets nas chamadas de folha de estilo e scripts, posteriormente processa-las utilizando o laravel-mix

--- resources/views/front/includes/menu.blade.php
+++ resources/views/front/includes/menu.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+        <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
              style="opacity: .8">
         <span class="brand-text font-weight-light">Ticket</span>
     </a>
@@ -11,10 +11,10 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Usuario e Silva</a>
+                <a href="#" class="d-block">Joao da Silva</a>
             </div>
         </div>
 
@@ -24,32 +24,61 @@
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
 
-                <li class="nav-header">Inventario</li>
+                <li class="nav-header">Administração</li>
                 <li class="nav-item has-treeview">
                     <a href="#" class="nav-link">
                         <i class="nav-icon far fa-plus-square"></i>
                         <p>
-                            Equipamentos
+                            Setores
                             <i class="fas fa-angle-left right"></i>
                         </p>
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="pages/examples/login.html" class="nav-link">
+                            <a href="{{url('/sectors/create')}}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Novo Equipamento</p>
+                                <p>Novo Setor</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="pages/examples/register.html" class="nav-link">
+                            <a href="{{'/sectors'}}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Listar Equipamentos</p>
+                                <p>Listar Setores</p>
                             </a>
                         </li>
                         <li class="nav-item">
                             <a href="pages/examples/forgot-password.html" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Descarga de Equipamentos</p>
+                                <p>Desativar Setor</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item has-treeview">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon far fa-plus-square"></i>
+                        <p>
+                            Usuarios
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{url('/sectors/create')}}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Novo Setor</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{'/sectors'}}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Listar Setores</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="pages/examples/forgot-password.html" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Desativar Setor</p>
                             </a>
                         </li>
                     </ul>
