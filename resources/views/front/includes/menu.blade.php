fe55425a1068691d452ef44a13a7e57af777d588
Raphael Mota <rphmota@gmail.com>
2020-07-31 19:55:04 -0300

    Update menu.blade.php
    adicionado link para equipamentos

--- resources/views/front/includes/menu.blade.php
+++ resources/views/front/includes/menu.blade.php
@@ -70,7 +70,7 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="{{'/sectors'}}" class="nav-link">
+                            <a href="{{route('devices.index')}}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Listar Equipamentos</p>
                             </a>
